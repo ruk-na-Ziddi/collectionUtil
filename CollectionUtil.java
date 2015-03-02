21637714b6bc54495579634895e38bbfe53de027
Ankur Singh <ankurks@thoughtworks.com>
2015-03-02 10:06:44 +0530

    reduce method is written tested for integer type adding all array elements

--- CollectionUtil.java
+++ CollectionUtil.java
@@ -1,28 +1,39 @@
 import java.util.*;
 
-interface Mapper<E,K> {
+interface Mapper<E,K>{
 	K mapperCallback(E element);
 }
 
-interface Filter<E> {
+interface Filter<E>{
 	boolean filterCallback(E element);
 }
 
-public class CollectionUtil {
-	public static<E,K> List<K> map(List<E> list, Mapper<E,K> listMapper) {
-		List<K> mapped = new ArrayList<K>();
+interface Reducer<E,K>{
+	K reduceCallback(K pv, E cv);
+}
+
+public class CollectionUtil{
+	public static<E,K> List<K> map(List<E> list, Mapper<E,K> listMapper){
+		List<K> mapped=new ArrayList<K>();
 		for(E number : list){
 			mapped.add(listMapper.mapperCallback(number));
 		}
 		return mapped;
 	}
 
-	public static<E> List<E> filter(List<E> list, Filter<E> listFilter) {
-		List<E> filtered = new ArrayList<E>();
+	public static<E> List<E> filter(List<E> list, Filter<E> listFilter){
+		List<E> filtered=new ArrayList<E>();
 		for(E number : list){
 			if((boolean)listFilter.filterCallback(number)) filtered.add(number);
 		}
 		return filtered;
+	}
+
+	public static<E,K> K reduce(List<E> list, Reducer<E,K> listReducer,K initialValue){
+		for(E ele : list) {
+			initialValue=listReducer.reduceCallback(initialValue,ele);
+		}
+		return initialValue;
 	}
 
 }
--- CollectionUtilTest.java
+++ CollectionUtilTest.java
@@ -2,33 +2,40 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.*;
 
-class AddTwoMapper implements Mapper<Integer,Integer> {
+class AddTwoMapper implements Mapper<Integer,Integer>{
 	public Integer mapperCallback(Integer number){
 		return number+2;
 	}
 }
 
-class AddABCMapper implements Mapper<String,String> {
+class AddABCMapper implements Mapper<String,String>{
 	public String mapperCallback(String string){
 		return string+"ABC";
 	}
 }
 
-class EvenFilter implements Filter<Integer> {
+class EvenFilter implements Filter<Integer>{
 	public boolean filterCallback(Integer number){
 		return number%2==0;
 	}
 }
 
+class AddAllReducer implements Reducer<Integer,Integer>{
+	public Integer reduceCallback(Integer pv,Integer cv){
+		return pv+cv;
+	}
+}
+
+
 public class CollectionUtilTest{
 	@Test
 	public void map_returns_an_Integer_List_of_each_element_increment_by_2(){
-		Mapper<Integer,Integer> listMapper = new AddTwoMapper();
-		List<Integer> numbers = new ArrayList<Integer>();
+		Mapper<Integer,Integer> listMapper=new AddTwoMapper();
+		List<Integer> numbers=new ArrayList<Integer>();
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
-		List<Integer> mapped = CollectionUtil.<Integer,Integer>map(numbers,listMapper);
+		List<Integer> mapped=CollectionUtil.<Integer,Integer>map(numbers,listMapper);
 		assertEquals(3,(int)mapped.get(0));
 		assertEquals(4,(int)mapped.get(1));
 		assertEquals(5,(int)mapped.get(2));
@@ -36,12 +43,12 @@ public class CollectionUtilTest{
 
 	@Test
 	public void map_returns_an_Integer_List_of_each_element_added_ABC_to_it(){
-		Mapper<String,String> listMapper = new AddABCMapper();
-		List<String> strings = new ArrayList<String>();
+		Mapper<String,String> listMapper=new AddABCMapper();
+		List<String> strings=new ArrayList<String>();
 		strings.add("abc");
 		strings.add("def");
 		strings.add("ghi");
-		List<String> mapped = CollectionUtil.<String,String>map(strings,listMapper);
+		List<String> mapped=CollectionUtil.<String,String>map(strings,listMapper);
 		assertEquals("abcABC",(String)mapped.get(0));
 		assertEquals("defABC",(String)mapped.get(1));
 		assertEquals("ghiABC",(String)mapped.get(2));
@@ -49,14 +56,28 @@ public class CollectionUtilTest{
 
 	@Test
 	public void filter_returns_an_Integer_List_of_even_elements(){
-		Filter<Integer> listFilter = new EvenFilter();
-		List<Integer> numbers = new ArrayList<Integer>();
+		Filter<Integer> listFilter=new EvenFilter();
+		List<Integer> numbers=new ArrayList<Integer>();
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
 		numbers.add(4);
-		List<Integer> filtered = CollectionUtil.<Integer>filter(numbers,listFilter);
+		List<Integer> filtered=CollectionUtil.<Integer>filter(numbers,listFilter);
 		assertEquals(2,(int)filtered.get(0));
 		assertEquals(4,(int)filtered.get(1));
+	}
+
+	@Test
+	public void reduce_gives_added_value_of_all_elements_of_list(){
+		Reducer<Integer,Integer>listReducer=new AddAllReducer();
+		List<Integer> numbers=new ArrayList<Integer>();
+		Integer initialValue=0;
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		Integer sumOfAllEle=CollectionUtil.<Integer,Integer>reduce(numbers,listReducer,initialValue);
+		assertEquals(15,(int)sumOfAllEle);
 	}
 }
